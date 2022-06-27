1d31182417673d614822f9648ad9b1c0b1151ca4
FtobarV64 <ftobarv@gmail.com>
2022-06-27 15:19:33 -0400

    configuring devise in the project

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
   devise_for :friends
-  get 'home/index'
+
   resources :assemblies
   resources :strains
   resources :wines
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root to: "home#index"
 
 end
