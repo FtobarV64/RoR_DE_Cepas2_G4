9f7d909312fd03e6aa1ad1b30bb66dd82875eac6
FtobarV64 <ftobarv@gmail.com>
2022-06-27 19:43:05 -0400

    adding Eonologist

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_27_181810) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_27_234226) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_27_181810) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_friends_on_email", unique: true
     t.index ["reset_password_token"], name: "index_friends_on_reset_password_token", unique: true
+  end
+
+  create_table "oenologists", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.string "nationality"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "strains", force: :cascade do |t|
